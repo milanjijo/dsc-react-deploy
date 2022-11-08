93263eb4ab4affb26e1cc4ff30fad7382a3d0510
milanjijo <milanjijo2k02@gmail.com>
2022-11-08 17:40:10 +0530

    Name Added

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,7 +3,7 @@ import "./Landing.css";
 function Landing() {
     return (
         <h1 class="hero-container" id="hero-sec">
-            Welcome to this brand new React Site built by Jaison...
+            Welcome to this brand new React Site built by Milan...
         </h1>
     )
 }
